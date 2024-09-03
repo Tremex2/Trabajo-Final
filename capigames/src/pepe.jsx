3eecd46400583884626e9e9fd7d9cc244213e6df
Tiziano <antotizi39@gmail.com>
2024-09-03 14:31:06 -0300

    reinicio

--- capigames/src/pepe.jsx
+++
@@ -1,9 +0,0 @@
-export function Pepe() {
-    return(
-        <>
-        <h1>pepe es amor</h1>
-        <h2>pepe es vida xdddd</h2>
-        <h3>este es el seundo comit del pepep</h3>
-        </>
-    )
-}
