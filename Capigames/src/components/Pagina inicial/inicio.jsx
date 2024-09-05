d98120e2d92b817a3b97922c2df15f69e77e9f07
Tiziano <antotizi39@gmail.com>
2024-09-05 19:00:11 -0300

    inicio a la mitad

---
+++ Capigames/src/components/Pagina inicial/inicio.jsx
@@ -0,0 +1,30 @@
+import "./inicio.css"
+
+export function Inicio() {
+    return(
+        <>
+        <div id="tags" className="tags">
+            <div className="titulo" id="titulo">
+                <h1>Tags</h1>
+            </div>
+            <div className="caja-tags">
+                <div className="tag">
+                   <h2>Accion</h2>
+                </div>
+                <div className="tag">
+                <h2>terror psicologico</h2>
+                </div>
+                <div className="tag">
+                 <h2>primera persona</h2>
+                </div>
+                <div className="tag">
+                  <h2>2D</h2>
+                </div>
+                <div className="tag">
+                   <h2>corto</h2>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
