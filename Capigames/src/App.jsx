779189878a51286bb37011dc5c0d2c5e54a6f46b
Tiziano <antotizi39@gmail.com>
2024-09-08 15:02:30 -0300

    router dom

--- Capigames/src/App.jsx
+++ Capigames/src/App.jsx
@@ -1,11 +1,19 @@
 import { Inicio } from './components/Pagina inicial/inicio'
+import {BrowserRouter, Routes ,Route} from "react-router-dom"
 import { Menu } from './components/Menu/menu'
+import { Pagina1 } from './components/pagina 1/pagina1'
+
 function App() {
 
   return (
     <>
+    <BrowserRouter>
     <Menu/>
-    <Inicio/>
+    <Routes>
+      <Route path='/' element={<Inicio/>}></Route>
+      <Route path='/pagina-1' element={<Pagina1/>}></Route>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
