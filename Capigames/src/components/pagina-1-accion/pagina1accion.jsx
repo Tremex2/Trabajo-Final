19b674a10f7879c07d69dc6c9d3f7e11192f7d4b
Tiziano <antotizi39@gmail.com>
2024-09-08 17:30:43 -0300

    grid terminado

--- Capigames/src/components/pagina-1-accion/pagina1accion.jsx
+++ Capigames/src/components/pagina-1-accion/pagina1accion.jsx
@@ -1,7 +1,7 @@
 import { Inicio } from "../Pagina inicial/inicio";
 import "../Cards/card.css"
 import jsonData from "./juegosAccion1.json"
-import "../"
+import Imagen from "../../public/images/gta5.jpg"
 export function Pagina1Accion() {
     const gta = jsonData.GTA 
     return(
@@ -34,7 +34,7 @@ export function Pagina1Accion() {
             <div className="titulo"></div>
             </div>
             <div className="card">
-            <img src="" alt="no image" />
+            <img src={Imagen} alt="no image" className="image"/>
             <div className="titulo"><h4>{gta.titulo}</h4></div>
             </div>
         </div>
