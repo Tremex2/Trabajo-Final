e6d361eca3c42122c28d84e83a905cb7e9c96f4d
Tiziano <antotizi39@gmail.com>
2024-09-10 20:07:12 -0300

    cortos y 2D imagenes agregadas

--- Capigames/src/components/corto/pagina1corto.jsx
+++ Capigames/src/components/corto/pagina1corto.jsx
@@ -1,5 +1,15 @@
 import { Inicio } from "../Pagina inicial/inicio"
 import "../Cards/card.css"
+import hike from "../../public/corto/a-short-hike.png"
+import firewach from "../../public/corto/firewach.jpg" 
+import gris from "../../public/corto/gris.webp"
+import inside from "../../public/corto/INSIDE.jpg"
+import journey  from "../../public/corto/journey.jpg"
+import oxenfree from "../../public/corto/oxenfree.jpg"
+import begginers from "../../public/corto/the-begginers-guide.jpg"
+import moon from "../../public/corto/to-the-moon.jpg"
+import finch from "../../public/corto/what-remains-of-edith-finch.avif"
+
 export function Corto1() {
     
     
@@ -9,40 +19,40 @@ export function Corto1() {
        <div className="container">
         <div className="grid">
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>Red Dead Redemption II</h4></div>
+            <img src={hike} alt="" className="image" />
+            <div className="titulo"><h4>A short hike</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>CodeVein</h4></div>
+            <img src={firewach} alt="" className="image" />
+            <div className="titulo"><h4>Firewach</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>cyberpunk 2077</h4></div>
+            <img src={gris} alt="" className="image" />
+            <div className="titulo"><h4>Gris</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>dark souls</h4></div>
+            <img src={inside} alt="" className="image" />
+            <div className="titulo"><h4>INSIDE</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>doom eternal</h4></div>
+            <img src={journey} alt="" className="image" />
+            <div className="titulo"><h4>Journey</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>the last of us</h4></div>
+            <img src={oxenfree} alt="" className="image" />
+            <div className="titulo"><h4>Oxenfree</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>the last of us 2</h4></div>
+            <img src={begginers} alt="" className="image" />
+            <div className="titulo"><h4>The begginers guide</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>Ultrakill</h4></div>
+            <img src={moon} alt="" className="image" />
+            <div className="titulo"><h4>To the moon</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>gta</h4></div>
+            <img src={finch} alt="" className="image" />
+            <div className="titulo"><h4>What remains of edith finch</h4></div>
             </div>
         </div>
         </div>
