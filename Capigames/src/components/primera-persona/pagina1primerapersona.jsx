8771e17ff19f7bc6389c49f371ca754c3005a8be
Tiziano <antotizi39@gmail.com>
2024-09-10 20:37:18 -0300

    tags iniciales terminados

--- Capigames/src/components/primera-persona/pagina1primerapersona.jsx
+++ Capigames/src/components/primera-persona/pagina1primerapersona.jsx
@@ -1,5 +1,16 @@
 import { Inicio } from "../Pagina inicial/inicio"
 import "../Cards/card.css"
+import bioshock from "../../public/primera-persona/bioshock.jpg"
+import dishonored from "../../public/primera-persona/dishonored.jpg"
+import dyingLight from "../../public/primera-persona/dying-light.jpg"
+import farCry from "../../public/primera-persona/far-cry-5.jpg"
+import halfLife from "../../public/primera-persona/half-life-II.jpg"
+import portalII from "../../public/primera-persona/portal-II.avif"
+import prey from "../../public/primera-persona/prey.jpg"
+import subnautica from "../../public/primera-persona/subnautica.jpg"
+import theEldersScroll from "../../public/primera-persona/the-elders-scroll.jpg"
+
+
 export function PrimeraPersona1() {
     return(
         <>
@@ -7,40 +18,40 @@ export function PrimeraPersona1() {
        <div className="container">
         <div className="grid">
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>Red Dead Redemption II</h4></div>
+            <img src={bioshock} alt="" className="image" />
+            <div className="titulo"><h4>Bioshock</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>CodeVein</h4></div>
+            <img src={dishonored} alt="" className="image" />
+            <div className="titulo"><h4>Dishonored</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>cyberpunk 2077</h4></div>
+            <img src={dyingLight} alt="" className="image" />
+            <div className="titulo"><h4>Dying Light</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>dark souls</h4></div>
+            <img src={farCry} alt="" className="image" />
+            <div className="titulo"><h4>Far Cry</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>doom eternal</h4></div>
+            <img src={halfLife} alt="" className="image" />
+            <div className="titulo"><h4>Half Life 2</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>the last of us</h4></div>
+            <img src={portalII} alt="" className="image" />
+            <div className="titulo"><h4>Portal II</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>the last of us 2</h4></div>
+            <img src={prey} alt="" className="image" />
+            <div className="titulo"><h4>Prey</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>Ultrakill</h4></div>
+            <img src={subnautica} alt="" className="image" />
+            <div className="titulo"><h4>Subnautica</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>gta</h4></div>
+            <img src={theEldersScroll} alt="" className="image" />
+            <div className="titulo"><h4>The elders scroll V: Skyrim</h4></div>
             </div>
         </div>
         </div>
--- Capigames/src/components/terror/pagina1terror.jsx
+++ Capigames/src/components/terror/pagina1terror.jsx
@@ -1,5 +1,16 @@
 import { Inicio } from "../Pagina inicial/inicio"
 import "../Cards/card.css"
+import amnesia from "../../public/terror/amnesia.webp"
+import layersOfFear from "../../public/terror/layers-of-fear.webp"
+import littleNightmaresII from "../../public/terror/little nightmares-II.jpg"
+import littleNightmares from "../../public/terror/little nightmares.jpg"
+import outlastII from "../../public/terror/outlast-II.jpg"
+import outlast from "../../public/terror/outlast.jpg"
+import phasmophobia from "../../public/terror/phasmophobia.jpg"
+import silentHillII from "../../public/terror/silent-hill-2.jpg"
+import soma from "../../public/terror/soma.jpg"
+
+
 
 export function Terror1() {
     return(
@@ -8,40 +19,40 @@ export function Terror1() {
        <div className="container">
         <div className="grid">
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>Red Dead Redemption II</h4></div>
+            <img src={amnesia} alt="" className="image" />
+            <div className="titulo"><h4>Amnesia</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>CodeVein</h4></div>
+            <img src={layersOfFear} alt="" className="image" />
+            <div className="titulo"><h4></h4>Layers of fear</div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>cyberpunk 2077</h4></div>
+            <img src={littleNightmaresII} alt="" className="image" />
+            <div className="titulo"><h4>Little nightmares II</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>dark souls</h4></div>
+            <img src={littleNightmares} alt="" className="image" />
+            <div className="titulo"><h4>Little nightmares</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>doom eternal</h4></div>
+            <img src={outlastII} alt="" className="image" />
+            <div className="titulo"><h4>Outlast II</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>the last of us</h4></div>
+            <img src={outlast} alt="" className="image" />
+            <div className="titulo"><h4>Outlast</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>the last of us 2</h4></div>
+            <img src={phasmophobia} alt="" className="image" />
+            <div className="titulo"><h4>Phasmophobia</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>Ultrakill</h4></div>
+            <img src={silentHillII} alt="" className="image" />
+            <div className="titulo"><h4>Silent Hill II</h4></div>
             </div>
             <div className="card">
-            <img src="" alt="" className="image" />
-            <div className="titulo"><h4>gta</h4></div>
+            <img src={soma} alt="" className="image" />
+            <div className="titulo"><h4>SOMA</h4></div>
             </div>
         </div>
         </div>
