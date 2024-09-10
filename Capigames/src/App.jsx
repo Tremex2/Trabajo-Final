d4b7b5429c14de161f1a87a466269cfdac86c76b
Tiziano <antotizi39@gmail.com>
2024-09-10 18:32:52 -0300

    imagenes

--- Capigames/src/App.jsx
+++ Capigames/src/App.jsx
@@ -1,8 +1,11 @@
 import { Inicio } from './components/Pagina inicial/inicio'
 import {BrowserRouter, Routes ,Route} from "react-router-dom"
 import { Menu } from './components/Menu/menu'
-import { Pagina1Accion } from './components/pagina-1-accion/pagina1accion'
-
+import { Pagina1Accion } from './components/accion/pagina1accion'
+import { Corto1 } from './components/corto/pagina1corto'
+import { SD1 } from './components/2D/pagina12D'
+import { Terror1 } from './components/terror/pagina1terror'
+import { PrimeraPersona1 } from './components/primera-persona/pagina1primerapersona'
 function App() {
 
   return (
@@ -11,7 +14,11 @@ function App() {
     <Menu/>
     <Routes>
       <Route path='/' element={<Inicio/>}></Route>
-      <Route path='/pagina-1/accion' element={<Pagina1Accion/>}></Route>
+      <Route path='/accion/1' element={<Pagina1Accion/>}></Route>
+      <Route path='/corto/1' element={<Corto1/>}></Route>
+      <Route path='/2D/1' element={<SD1/>}></Route>
+      <Route path='/terror/1' element={<Terror1/>}></Route>
+      <Route path='/primera-persona/1' element={<PrimeraPersona1/>}></Route>
     </Routes>
     </BrowserRouter>
     </>
---
+++ Capigames/src/components/2D/pagina12D.jsx
@@ -0,0 +1,50 @@
+import { Inicio } from "../Pagina inicial/inicio"
+import "../Cards/card.css"
+
+export function SD1() {
+    return(
+        <>
+       <Inicio/>
+       <div className="container">
+        <div className="grid">
+            <div className="card">
+            <img src="" alt="" className="image" />
+            <div className="titulo"><h4>Red Dead Redemption II</h4></div>
+            </div>
+            <div className="card">
+            <img src="" alt="" className="image" />
+            <div className="titulo"><h4>CodeVein</h4></div>
+            </div>
+            <div className="card">
+            <img src="" alt="" className="image" />
+            <div className="titulo"><h4>cyberpunk 2077</h4></div>
+            </div>
+            <div className="card">
+            <img src="" alt="" className="image" />
+            <div className="titulo"><h4>dark souls</h4></div>
+            </div>
+            <div className="card">
+            <img src="" alt="" className="image" />
+            <div className="titulo"><h4>doom eternal</h4></div>
+            </div>
+            <div className="card">
+            <img src="" alt="" className="image" />
+            <div className="titulo"><h4>the last of us</h4></div>
+            </div>
+            <div className="card">
+            <img src="" alt="" className="image" />
+            <div className="titulo"><h4>the last of us 2</h4></div>
+            </div>
+            <div className="card">
+            <img src="" alt="" className="image" />
+            <div className="titulo"><h4>Ultrakill</h4></div>
+            </div>
+            <div className="card">
+            <img src="" alt="" className="image" />
+            <div className="titulo"><h4>gta</h4></div>
+            </div>
+        </div>
+        </div>
+        </>
+    )
+}
