b10c0429f52a17f0ef1f5b6c75ede19dd0ff1eb3
Tiziano <antotizi39@gmail.com>
2024-09-12 20:52:03 -0300

    error 404 terminado
    Co-authored-by: davidburzo <davidburzo@users.noreply.github.com>

--- Capigames/vite.config.js
+++ Capigames/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite el acceso desde cualquier IP
+    port: 5173, // El puerto que estás usando
+  },
 })
