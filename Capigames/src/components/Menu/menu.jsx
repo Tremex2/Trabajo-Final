4e1538c0c0f76d5a7e849e07e972f2dda69543d2
Tiziano <antotizi39@gmail.com>
2024-09-21 11:25:32 -0300

    rework al menu

--- Capigames/src/components/Menu/menu.jsx
+++ Capigames/src/components/Menu/menu.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './menu.css';
 import { Link } from 'react-router-dom';
 
 
 export function Menu() {
   const [menuClass, setMenuClass] = useState('izquierda');
+  const menuRef = useRef(null);
 
   function despliegue() {
     setMenuClass(prevClass => prevClass === 'izquierda' ? 'derecha' : 'izquierda');
   }
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuClass('izquierda');
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
@@ -17,8 +31,9 @@ export function Menu() {
         <div className="barra2"></div>
         <div className="barra3"></div>
       </div>
-      <div className={menuClass} id="menu">
+      <div className={menuClass} id="menu" ref={menuRef}>
         <Link className='menu'><h2> Menu</h2></Link>
+        <div className="linea-recta-menu"></div>
         <Link className="opcion" to={"/"}><h4>Inicio</h4></Link>
         <Link className="opcion"><h4>opcion</h4></Link>
         <Link className="opcion"><h4>opcion</h4></Link>
@@ -29,6 +44,7 @@ export function Menu() {
         <Link className="opcion"><h4>opcion</h4></Link>
         <Link className="opcion"><h4>opcion</h4></Link>
         <Link className="opcion"><h4>opcion</h4></Link>
+        <div className="linea-recta-cerrar"></div>
         <Link className="boton-menu-cerrar" onClick={despliegue}><h2>Cerrar</h2></Link>
       </div>
     </>
