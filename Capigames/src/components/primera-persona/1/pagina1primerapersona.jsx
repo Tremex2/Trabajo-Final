0fb1218494e827fc6337f4291175b32875c5fc03
Tiziano <antotizi39@gmail.com>
2024-09-21 11:56:15 -0300

    cambiar divs por Link

--- Capigames/src/components/primera-persona/1/pagina1primerapersona.jsx
+++ Capigames/src/components/primera-persona/1/pagina1primerapersona.jsx
@@ -15,55 +15,55 @@ import theEldersScroll from "../../../public/primera-persona/the-elders-scroll.j
 
 
 export function PrimeraPersona1() {
-    return(
+    return (
         <>
-       <Inicio/>
-       <div className="container">
-        <div className="grid">
-            <div className="card">
-            <img src={bioshock} alt="" className="image" />
-            <div className="titulo"><h4>Bioshock</h4></div>
+            <Inicio />
+            <div className="container">
+                <div className="grid">
+                    <div className="card">
+                        <img src={bioshock} alt="" className="image" />
+                        <Link className="titulo"><h4>Bioshock</h4></Link>
+                    </div>
+                    <div className="card">
+                        <img src={dishonored} alt="" className="image" />
+                        <Link className="titulo"><h4>Dishonored</h4></Link>
+                    </div>
+                    <div className="card">
+                        <img src={dyingLight} alt="" className="image" />
+                        <Link className="titulo"><h4>Dying Light</h4></Link>
+                    </div>
+                    <div className="card">
+                        <img src={farCry} alt="" className="image" />
+                        <Link className="titulo"><h4>Far Cry</h4></Link>
+                    </div>
+                    <div className="card">
+                        <img src={halfLife} alt="" className="image" />
+                        <Link className="titulo"><h4>Half Life 2</h4></Link>
+                    </div>
+                    <div className="card">
+                        <img src={portalII} alt="" className="image" />
+                        <Link className="titulo"><h4>Portal II</h4></Link>
+                    </div>
+                    <div className="card">
+                        <img src={prey} alt="" className="image" />
+                        <Link className="titulo"><h4>Prey</h4></Link>
+                    </div>
+                    <div className="card">
+                        <img src={subnautica} alt="" className="image" />
+                        <Link className="titulo"><h4>Subnautica</h4></Link>
+                    </div>
+                    <div className="card">
+                        <img src={theEldersScroll} alt="" className="image" />
+                        <Link className="titulo"><h4>The elders scroll V: Skyrim</h4></Link>
+                    </div>
+                </div>
             </div>
-            <div className="card">
-            <img src={dishonored} alt="" className="image" />
-            <div className="titulo"><h4>Dishonored</h4></div>
+            <div className="paginas">
+                <NavLink className="pagina" to={"/primera-persona/1"}>1</NavLink>
+                <NavLink className="pagina" to={"/primera-persona/2"}>2</NavLink>
+                <NavLink className="pagina" to={"/primera-persona/3"}>3</NavLink>
+                <NavLink className="pagina" to={"/primera-persona/4"}>4</NavLink>
             </div>
-            <div className="card">
-            <img src={dyingLight} alt="" className="image" />
-            <div className="titulo"><h4>Dying Light</h4></div>
-            </div>
-            <div className="card">
-            <img src={farCry} alt="" className="image" />
-            <div className="titulo"><h4>Far Cry</h4></div>
-            </div>
-            <div className="card">
-            <img src={halfLife} alt="" className="image" />
-            <div className="titulo"><h4>Half Life 2</h4></div>
-            </div>
-            <div className="card">
-            <img src={portalII} alt="" className="image" />
-            <div className="titulo"><h4>Portal II</h4></div>
-            </div>
-            <div className="card">
-            <img src={prey} alt="" className="image" />
-            <div className="titulo"><h4>Prey</h4></div>
-            </div>
-            <div className="card">
-            <img src={subnautica} alt="" className="image" />
-            <div className="titulo"><h4>Subnautica</h4></div>
-            </div>
-            <div className="card">
-            <img src={theEldersScroll} alt="" className="image" />
-            <div className="titulo"><h4>The elders scroll V: Skyrim</h4></div>
-            </div>
-        </div>
-        </div>
-        <div className="paginas">
-        <NavLink className="pagina" to={"/primera-persona/1"}>1</NavLink>
-        <NavLink className="pagina" to={"/primera-persona/2"}>2</NavLink>
-        <NavLink className="pagina" to={"/primera-persona/3"}>3</NavLink>
-        <NavLink className="pagina" to={"/primera-persona/4"}>4</NavLink>
-        </div>
         </>
     )
 }
